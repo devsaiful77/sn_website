df7eb37d0c33e7570a78eebffd5456f0922ff9e0
Saiful Islam <104412703+devsaiful77@users.noreply.github.com>
2026-08-22 22:05:38 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ Route::get('/{any}', function () {
     return view('app');
 })->where('any', '.*');
 
-// Route::get('/{any?}', fn () => view('app'))->where('any', '.*');
+// Route::get('/{any?}', fn () => view('app'))->where('any', '.  ****');
